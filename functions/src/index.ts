e57068303e5879d1419745850cd453435eb01a7d
Jayden Boesch <jayden.boesch@gmail.com>
2021-05-12 13:35:37 -0500

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,16 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+//const cors = require('cors')({origin: true});
+
+admin.initializeApp();
+
+export const helloWorld = functions.https.onCall(async (data, context) => {
+  // cors(request, response, async () => {
+  //   const msg = request.query.text;
+  //   const writeResult = await admin.firestore().collection('debug').add({message: msg});
+  //   response.json({result: `Message with ID: ${writeResult.id} added.`});
+  // });
+  const text = data.text;
+  const writeResult = await admin.firestore().collection('debug').add({message: text});
+  return {result: `Message with ID: ${writeResult.id} added.`};
+});
